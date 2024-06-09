e3bbf9f737037d724e827baa0b064630d7b6b901
n3-tsuguchi <142201478+n3-tsuguchi@users.noreply.github.com>
2024-06-09 12:43:31 +0900

    Update sample_spec.rb

--- serverspec/spec/18.179.174.216/sample_spec.rb
+++ serverspec/spec/18.179.174.216/sample_spec.rb
@@ -58,7 +58,7 @@ end
 
 describe 'SSH connection' do
   it 'should connect to the remote host' do
-    Net::SSH.start('43.207.74.110', 'ec2-user', keys: ['kawakami.pem']) do |ssh|
+    Net::SSH.start('18.179.174.216', 'ec2-user', keys: ['kawakami.pem']) do |ssh|
       # ここにSSH接続が成功した場合のテストコードを記述する
       # 例えば、特定のコマンドを実行して結果を検証するなど
     end
