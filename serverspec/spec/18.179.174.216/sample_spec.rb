aa23d86f5b4b0185da0ef264c854d775fd65bf27
n3-tsuguchi <142201478+n3-tsuguchi@users.noreply.github.com>
2024-06-09 18:59:06 +0900

    Update sample_spec.rb

--- serverspec/spec/18.179.174.216/sample_spec.rb
+++ serverspec/spec/18.179.174.216/sample_spec.rb
@@ -5,24 +5,24 @@ set :host, '18.179.174.216'
 set :ssh_options, :user => 'ec2-user', :keys => ['kawakami.pem']
 
 listen_port = 80
+rbenv_path = '/home/ec2-user/.rbenv/shims:$PATH'
 
-describe command('ruby') do
-  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+describe command('ruby -v') do
+  let(:path) { rbenv_path }
   its(:stdout) { should match /ruby 3.1.2/ }
 end
 
 describe package('bundler') do
-  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+  let(:path) { rbenv_path }
   it { should be_installed.by('gem').with_version('2.3.14') }
 end
 
 describe package('rails') do
-  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+  let(:path) { rbenv_path }
   it { should be_installed.by('gem').with_version('7.1.3.2') }
 end
 
-describe command('node') do
-  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+describe command('node -v') do
   its(:exit_status) { should eq 0 }
 end
 
@@ -31,7 +31,7 @@ describe service('nginx') do
 end
 
 describe package('unicorn') do
-  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+  let(:path) { rbenv_path }
   it { should be_installed.by('gem') }
 end
 
@@ -46,5 +46,3 @@ end
 describe command("curl http://127.0.0.1:#{listen_port}/ -o /dev/null -w \"%{http_code}\\n\" -s") do
   its(:stdout) { should match /^200$/ }
 end
-
-
