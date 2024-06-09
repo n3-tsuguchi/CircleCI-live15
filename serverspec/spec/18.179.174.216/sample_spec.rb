17b362b7e1c58c1f09cf14b33c67b5bd99f522eb
n3-tsuguchi <142201478+n3-tsuguchi@users.noreply.github.com>
2024-06-09 13:57:18 +0900

    Update sample_spec.rb

--- serverspec/spec/18.179.174.216/sample_spec.rb
+++ serverspec/spec/18.179.174.216/sample_spec.rb
@@ -6,27 +6,27 @@ set :ssh_options, :user => 'ec2-user', :keys => ['kawakami.pem']
 
 listen_port = 80
 
-describe command('ruby --version') do
+describe command('ruby') do
   let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
   its(:stdout) { should match /ruby 3.1.2/ }
 end
 
-describe package('bundler --version') do
+describe package('bundler') do
   let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
   it { should be_installed.by('gem').with_version('2.3.14') }
 end
 
-describe package('rails --version') do
+describe package('rails') do
   let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
   it { should be_installed.by('gem').with_version('7.1.3.2') }
 end
 
-describe command('node -v') do
+describe command('node') do
   let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
   its(:exit_status) { should eq 0 }
 end
 
-describe command('yarn -v') do
+describe command('yarn') do
   let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
   its(:exit_status) { should eq 0 }
 end
@@ -52,7 +52,7 @@ describe port(listen_port) do
   it { should be_listening }
 end
 
-describe command('curl http://127.0.0.1:#{listen_port}/ -o /dev/null -w "%{http_code}\n" -s') do
+describe command("curl http://127.0.0.1:#{listen_port}/ -o /dev/null -w \"%{http_code}\\n\" -s") do
   its(:stdout) { should match /^200$/ }
 end
 
